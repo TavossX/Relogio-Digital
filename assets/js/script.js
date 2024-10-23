daf2e5a869086d5d7b433b47f4bdfc7d37aaee47
Otavio <otaviomficho@gmail.com>
2024-10-23 14:40:52 -0300

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 const data = document.getElementById('data');
 const mes = document.getElementById('mes');
-const diaDaSemana = document.getElementById('dia-da-semana'); 
+const diaDaSemana = document.getElementById('dia-da-semana');
 
 const diasDaSemana = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
 
@@ -13,15 +13,17 @@ const relogio = setInterval(function time() {
     let min = dateToday.getMinutes();
     let seg = dateToday.getSeconds();
     let dat = dateToday.getDate();
-    let me = dateToday.getMonth();
-    let dia = diasDaSemana[dateToday.getDay()]; 
+    let me = dateToday.getMonth() + 1; // Corrige para 1-12
+    let dia = diasDaSemana[dateToday.getDay()];
 
+    // Formatação com dois dígitos
     if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
     if (seg < 10) seg = '0' + seg;
     if (dat < 10) dat = '0' + dat;
-    if (me < 9) me = '0' + (me + 1);
+    if (me < 10) me = '0' + me; // Formata o mês para ter dois dígitos
 
+    // Atualiza os elementos do DOM
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = seg;
